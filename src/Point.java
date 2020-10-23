24d2054eab6e28110ea39b49ea8c76a5a307d986
ViktorHagstrom <viktor.hagstrom@hotmail.se>
2020-10-23 11:40:15 +0200

    Initial commit

---
+++ src/Point.java
@@ -0,0 +1,11 @@
+/**
+ * Point är en klass som beskriver
+ * en punkt i ett koordinatsystem
+ */
+public class Point {
+
+
+    // Instansvariabler
+    double x; // x-koordinat
+    double y; // y-koordinat
+}
