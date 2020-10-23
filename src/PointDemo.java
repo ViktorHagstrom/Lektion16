1efc2acc5306eb38c11469344c43d73289f13b4a
ViktorHagstrom <viktor.hagstrom@hotmail.se>
2020-10-23 11:53:06 +0200

    Assigned values to p1 and p2 and printed to confirm values

--- src/PointDemo.java
+++ src/PointDemo.java
@@ -14,7 +14,19 @@ public class PointDemo {
         System.out.println("p2: " + p2);
 
         System.out.println("p1(" + p1.x + " , " + p1.y + ")"); // Formatering för kordinater p1(0.0 , 0.0)
-        System.out.println("p2(" + p2.x + " , " + p2.y + ")"); // Formatering för kordinater p2(0.0 , 0.0)
+        System.out.println("p2(" + p2.x + " , " + p2.y + ")"); // p2(0.0 , 0.0)
+
+        System.out.println("-------------");
+        // Dålig lösning, mer kommer i kap 15, smarta objekt
+        p1.x = 5; // flyttar x 5 steg åt höger
+        p1.y = -5; // flyttar y 5 steg nedåt
+
+        p2.x = 10.5;
+        p2.y = -5.5;
+
+        System.out.println("p1(" + p1.x + " , " + p1.y + ")"); // Formatering för kordinater p1(0.0 , 0.0)
+        System.out.println("p2(" + p2.x + " , " + p2.y + ")"); // p2(0.0 , 0.0)
+
 
     }
 }
